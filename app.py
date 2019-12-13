7587f41b66887282b99f4109570fa8b1816498bf
AIe !! <makramjandar@gmail.com>
2019-12-13 13:38:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -294,10 +294,10 @@ def loadDirectorSummary(Genre, Director, Movie):
     return table
 
 
-# start Flask server
+# start erver
 if __name__ == '__main__':
     app.run_server(
-        debug=True,
+        debug=False,
         host='0.0.0.0',
         port=8050
     )
